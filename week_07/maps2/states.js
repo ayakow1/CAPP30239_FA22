afc28b72d6ddf6f787512510e78ac4727f0a49f3
Ayako Watanabe <ayako.tomita.1024@gmail.com>
2022-11-29 14:52:55 -0600

    class

--- week_07/maps2/states.js
+++ week_07/maps2/states.js
@@ -1,18 +1,22 @@
 const height = 610,
   width = 975;
 
-const svg = d3.select("#chart")
+const svg = d3
+  .select("#chart")
   .append("svg")
   .attr("viewBox", [0, 0, width, height]);
 
-d3.json("libs/counties-albers-10m.json").then(us => {
-
-  const counties = topojson.feature(us, us.objects.counties); // Map simple geometries
+d3.json("libs/counties-albers-10m.json").then((us) => {
+  //const counties = topojson.feature(us, us.objects.counties); // Map simple geometries
+  //const counties = topojson.feature(us, us.objects.states); // Map simple geometries
+  const counties = topojson.feature(us, us.objects.nation); // Map simple geometries
+  console.log(us);
   console.log(counties);
 
   const path = d3.geoPath();
 
-  svg.append("g")
+  svg
+    .append("g")
     .selectAll("path")
     .data(counties.features)
     .join("path")
@@ -20,5 +24,4 @@ d3.json("libs/counties-albers-10m.json").then(us => {
     .attr("fill", "white")
     // .attr("fill", d => (d.id in dataById) ? color(dataById[d.id].rate) : '#ccc')
     .attr("d", path);
-
 });
